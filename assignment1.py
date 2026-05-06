a73ad3093526af6ec1b59650430f1d481b64eeea
Hristo Bizhev <bizhev.hristo@gmail.com>
2026-05-06 23:38:21 +0200

    message types and on_response

--- assignment1.py
+++ assignment1.py
@@ -2,9 +2,76 @@ import argparse
 import asyncio
 import hashlib
 import time
-from ipv8.configuration import ConfigBuilder
+from ipv8.configuration import ConfigBuilder, Strategy, WalkerDefinition, default_bootstrap_defs
+from ipv8.community import Community
+from ipv8.peer import Peer
+from ipv8.peerdiscovery.network import PeerObserver
 from ipv8_service import IPv8
+from ipv8.lazy_community import lazy_wrapper
+from ipv8.messaging.lazy_payload import VariablePayload, vp_compile
 
+@vp_compile
+class SubmissionPayload(VariablePayload):
+    msg_id = 1
+
+    format_list = ["varlenHutf8", "varlenHutf8", "q"]
+    names = ["email", "github_url", "nonce"]
+
+
+@vp_compile
+class ResponsePayload(VariablePayload):
+    msg_id = 2
+
+    format_list = ["?", "varlenHutf8"]
+    names = ["success", "message"]
+
+class LabCommunity(Community, PeerObserver):
+    community_id = b""
+    server_public_key = b""
+
+    def __init__(self, settings):
+        super().__init__(settings)
+        self.add_message_handler(ResponsePayload, self.on_response)
+
+    def started(self):
+        print("Joining Lab community.")
+        self.register_task(
+            "find_server",
+            self.find_server,
+            interval=2.0,
+            delay=0,
+        )
+
+    async def find_server(self):
+        for peer in self.get_peers():
+            if peer.public_key.key_to_bin() == self.server_public_key:
+                print("Verified server found:", peer.address)
+                return
+            
+    def on_peer_added(self, peer: Peer):
+        peer_public_key = peer.public_key.key_to_bin()
+
+        print("Discovered peer:", peer_public_key.hex())
+
+        if peer_public_key == self.server_public_key:
+            print()
+            print("Verified server found!")
+            print("Server address:", peer.address)
+            print("Server public key:", peer_public_key.hex())
+            print()
+            return
+
+    @lazy_wrapper(ResponsePayload)
+    def on_response(self, peer, payload):
+        peer_public_key = peer.public_key.key_to_bin()
+
+        if peer_public_key != self.server_public_key:
+            print("Ignored response from non-server peer.")
+            return
+
+        print("Server response received.")
+        print("Success:", payload.success)
+        print("Message:", payload.message)
 
 async def main():
     parser = argparse.ArgumentParser()
@@ -17,13 +84,21 @@ async def main():
     parser.add_argument(
         "--email",
         required=True,
-        help="Your official TU Delft email address.",
     )
 
     parser.add_argument(
         "--github-url",
         required=True,
-        help="The public GitHub repository URL for this lab.",
+    )
+
+    parser.add_argument(
+    "--community-id",
+    required=True,
+    )
+
+    parser.add_argument(
+    "--server-public-key",
+    required=True,
     )
 
     args = parser.parse_args()
@@ -35,6 +110,15 @@ async def main():
     if (len(args.github_url) == 0) or len(args.github_url.encode("utf-8")) > 512:
         print("Invalid GitHub repository URL. Please provide a valid URL.")
         return
+    
+    if len(args.community_id) != 40:
+        raise ValueError("Community ID must be 40 hex characters.")
+
+    if len(args.server_public_key) != 148:
+        raise ValueError("Server public key must be 148 hex characters.")
+
+    LabCommunity.community_id = bytes.fromhex(args.community_id)
+    LabCommunity.server_public_key = bytes.fromhex(args.server_public_key)
 
     email_bytes = args.email.replace("\n", "").encode("utf-8")
     github_url_bytes = args.github_url.replace("\n", "").encode("utf-8")
@@ -49,7 +133,27 @@ async def main():
         args.key_file,
     )
 
-    ipv8 = IPv8(builder.finalize())
+    builder.add_overlay(
+        "LabCommunity",
+        "lab-key",
+        [
+            WalkerDefinition(
+                Strategy.RandomWalk,
+                20,
+                {"timeout": 3.0},
+            )
+        ],
+        default_bootstrap_defs,
+        {},
+        [("started",)],
+    )
+
+    ipv8 = IPv8(
+        builder.finalize(),
+        extra_communities={
+            "LabCommunity": LabCommunity,
+        },
+    )
 
     await ipv8.start()
 
@@ -83,7 +187,13 @@ async def main():
 
         nonce += 1
 
-    await ipv8.stop()
+    try:
+        while True:
+            await asyncio.sleep(1)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        await ipv8.stop()
 
 
 if __name__ == "__main__":
