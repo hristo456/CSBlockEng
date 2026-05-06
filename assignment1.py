459ce22149b3c86bb1134b3277db5d42f1766370
Hristo Bizhev <bizhev.hristo@gmail.com>
2026-05-06 22:52:09 +0200

    hash computation

--- assignment1.py
+++ assignment1.py
@@ -1,6 +1,7 @@
 import argparse
 import asyncio
-
+import hashlib
+import time
 from ipv8.configuration import ConfigBuilder
 from ipv8_service import IPv8
 
@@ -9,6 +10,7 @@ async def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--key-file",
+        required=True,
         help="Private key. Don't commit.",
     )
 
@@ -26,11 +28,21 @@ async def main():
 
     args = parser.parse_args()
 
+    if ((not args.email.endswith("@tudelft.nl")) and (not args.email.endswith("@student.tudelft.nl"))) or len(args.email.encode("utf-8")) > 254:
+        print("Invalid email address. Please provide a valid TU Delft email address.")
+        return
+    
+    if (len(args.github_url) == 0) or len(args.github_url.encode("utf-8")) > 512:
+        print("Invalid GitHub repository URL. Please provide a valid URL.")
+        return
+
+    email_bytes = args.email.replace("\n", "").encode("utf-8")
+    github_url_bytes = args.github_url.replace("\n", "").encode("utf-8")
+
     # IPv8 configuration.
     builder = ConfigBuilder().clear_keys().clear_overlays()
 
-    # If the key file does not exist, IPv8 will create it.
-    # If it already exists, IPv8 will load the same identity again.
+    # Key loader
     builder.add_key(
         "lab-key",
         "curve25519",
@@ -41,12 +53,35 @@ async def main():
 
     await ipv8.start()
 
-    # The private key stays in the .pem file.
-    # This prints only the public key, which is safe to share.
     peer = ipv8.keys["lab-key"]
     print("IPv8 identity loaded.")
     print("Private key file:", args.key_file)
     print("Public key:", peer.public_key.key_to_bin().hex())
+
+    hash_prefix = email_bytes + b"\n" + github_url_bytes + b"\n"
+
+    print("Starting local Proof of Work search...")
+    start_time = time.time()
+    nonce = 0
+
+    while True:
+        nonce_bytes = nonce.to_bytes(8)
+
+        digest = hashlib.sha256(hash_prefix + nonce_bytes).digest()
+
+        # 28 leading zero bits means: first byte is 0, second byte is 0, third byte is 0 and top 4 bits of fourth byte are 0
+        if digest[0] == 0 and digest[1] == 0 and digest[2] == 0 and digest[3] < 16:
+            elapsed = time.time() - start_time
+
+            print()
+            print("Found valid nonce!")
+            print("Nonce:", nonce)
+            print("Hash:", digest.hex())
+            print("Elapsed seconds:", round(elapsed, 2))
+
+            break
+
+        nonce += 1
 
     await ipv8.stop()
 
